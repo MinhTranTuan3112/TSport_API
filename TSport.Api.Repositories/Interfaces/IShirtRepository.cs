d085c13e203fb016a1b425daf47778ae51479dfe
Nguyễn Tấn Toàn <toanntse172018@fpt.edu.vn>
2024-06-02 09:54:39 +0700

    create repo

---
+++ TSport.Api.Repositories/Interfaces/IShirtRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSport.Api.Models.Entities;
+
+namespace TSport.Api.Repositories.Interfaces
+{
+    public interface IShirtRepository : IGenericRepository<Shirt>
+    {
+
+    }
+}
---
+++ TSport.Api.Repositories/Repositories/ShirtRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSport.Api.Models.Entities;
+using TSport.Api.Repositories.Interfaces;
+
+namespace TSport.Api.Repositories.Repositories
+{
+    public class ShirtRepository : GenericRepository<Shirt>, IShirtRepository
+    {
+        private readonly TsportDbContext _context;
+        public ShirtRepository(TsportDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
