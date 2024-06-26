822beed2566f84688771500ed548383c84893dd5
Nguyễn Tấn Toàn <toanntse172018@fpt.edu.vn>
2024-06-26 10:43:58 +0700

    use docker connection

--- TSport.Api/Extensions/IServiceCollectionExtensions.cs
+++ TSport.Api/Extensions/IServiceCollectionExtensions.cs
@@ -44,7 +44,7 @@ namespace TSport.Api.Extensions
 
         private static IServiceCollection AddDbContextWithConfigurations(this IServiceCollection services, IConfiguration configuration)
         {
-            string connectionString = configuration.GetConnectionString("DefaultConnection")!;
+            string connectionString = configuration.GetConnectionString("DockerConnection")!;
             services.AddDbContext<TsportDbContext>(options =>
             options.UseSqlServer(connectionString));
             return services;
