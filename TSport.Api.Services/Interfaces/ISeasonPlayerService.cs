9767af5ba8e8a0e3e9b1f4a9d3a6acd50c5e6b9c
MetRyan <101856114+MetRyan@users.noreply.github.com>
2024-07-19 18:26:07 +0700

    FixADdSeasonplayer

--- TSport.Api.Services/Interfaces/ISeasonPlayerService.cs
+++ TSport.Api.Services/Interfaces/ISeasonPlayerService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TSport.Api.Models.RequestModels.SeasonPLayer;
 using TSport.Api.Services.BusinessModels.SeasonPlayer;
 
 namespace TSport.Api.Services.Interfaces
@@ -9,5 +10,8 @@ namespace TSport.Api.Services.Interfaces
     public interface ISeasonPlayerService
     {
        Task<List<SeasonPlayerWithSeasonAndClubModel>> GetSeasonPlayers();
+
+        Task AddSeasonPlayer(SeasonPlayerRequest request);
+
     }
 }
--- TSport.Api.Services/Services/SeasonPlayerService.cs
+++ TSport.Api.Services/Services/SeasonPlayerService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Mapster;
 using Microsoft.EntityFrameworkCore;
+using TSport.Api.Models.RequestModels.SeasonPLayer;
+using TSport.Api.Repositories.Entities;
 using TSport.Api.Repositories.Interfaces;
 using TSport.Api.Services.BusinessModels.SeasonPlayer;
 using TSport.Api.Services.Interfaces;
@@ -17,6 +19,23 @@ namespace TSport.Api.Services.Services
         public SeasonPlayerService(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddSeasonPlayer(SeasonPlayerRequest request)
+        {
+            var seasonPlayer = new SeasonPlayer
+            {
+                // = request.Code,
+              SeasonId = request.Seasonid,
+              PlayerId =request.playerid,
+
+            };
+
+            await _unitOfWork.SeasonPlayerRepository.AddAsync(seasonPlayer);
+
+            await _unitOfWork.SaveChangesAsync();
+
+
         }
 
         public async Task<List<SeasonPlayerWithSeasonAndClubModel>> GetSeasonPlayers()
